4d36781ee7170b9feaceec5c01c31e6e7cd4b736
umarfauzan11 <survaivell@gmail.com>
2026-07-21 10:03:40 +0700

    US Update : fourth-push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: './src/index.css',  // Entry point CSS
+      output: {
+        entryFileNames: 'uwebly.min.css',  // ⭐ Custom filename!
+        assetFileNames: 'uwebly.min.css'
+      }
+    }
+  }
 })
